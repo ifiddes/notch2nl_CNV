237b571b26c74684ce95d00b3f9e87f5b47e9def
Ian Fiddes <ian.t.fiddes@gmail.com>
2015-04-28 20:01:17 -0700

    need a driver script

---
+++ pipeline.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+batchSystem="singleMachine"
+jobTree=".jobTree"
+maxThreads="10"
+log="test.log"
+queries="data/queries.pickle"
+genomes="GRCh37-lite"
+tissue_types="10"
+debug_cutoff="2"
+
+export PATH=./bin/:./sonLib/bin:./jobTree/bin:${PATH}
+export PYTHONPATH=./:${PYTHONPATH}
+
+if [ -d ${jobTree} ]; then
+    rm -rf ${jobTree}
+fi
+
+if [ -ne ${queries} ]; then
+    python src/cgQueryHandler.py --genomes ${genomes} --tissue_types ${tissue_types} --debug_cutoff ${debug_cutoff}
+fi
+
+python src/main.py --cgquery_file ${queries}
