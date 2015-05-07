a525e55c344b7b06cd7932325c9dc7d8c53325c5
Ian Fiddes <ian.t.fiddes@gmail.com>
2015-05-07 10:22:15 -0700

    no wiggle errors

--- src/tmp2.py
+++ src/tmp2.py
@@ -13,8 +13,15 @@ add_mole_to_graph(graph, unmasked_ref_path, masked_ref_path)
 #add_individual_to_graph(graph, fastq_path)
 ilp_model = KmerIlpModel(graph)
 normalizing_kmers = get_normalizing_kmers("data/kmer_model_data/normalizing.fa", 49)
-data_counts, normalizing = get_kmer_counts(graph, normalizing_kmers, "/home/ifiddes/hive/notch2nl_CNV/output/mike_sny/mike_sny.49mer.fa")
+data_counts, normalizing = get_kmer_counts(graph, normalizing_kmers, "output/mike_sny/mike_sny.49mer.fa")
 ilp_model.introduce_data(data_counts, normalizing)
 ilp_model.solve()
 result_dict = ilp_model.report_copy_map()
 raw_dict = ilp_model.report_normalized_raw_data_map()
+uuid = "test"
+out_raw_path = "test.wiggle"
+with open(out_raw_path, "w") as outf:
+    for para in raw_dict:
+        for start, stop, val in raw_dict[para]:
+            outf.write("variableStep chrom=chr1 span={}\n".format(stop - start))
+            outf.write("{} {}\n".format(start, val))
